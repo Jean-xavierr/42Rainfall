0070eed25d152fb299bda44d9a4a6c9d9dea8433
Jean-xavierr <jereligi@student.42.fr>
2022-11-17 14:05:06 +0100

    :arrow_up: Update level6

---
+++ level6/source.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void	n(void)
+{
+	system("/bin/cat /home/user/level7/.pass");
+    	return ;
+}
+
+
+void	m(void)
+{
+	puts("Nope");
+	return ;
+}
+
+
+int	main(int argc, char **argv)
+{
+	char	**var_one;
+	char	**var_two;
+
+	var_one = malloc(62);
+	var_two = (char **)malloc(4);
+	*var_two = m;
+	strcpy(var_one, argv[1]);
+	(**var_two)();
+	return 0;
+}
